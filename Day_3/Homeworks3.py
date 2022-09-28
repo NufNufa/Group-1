49cffcde012e21d60d5b7f57cc3b4d0a8897edae
NufNufa <gnashgab.game007@gmail.com>
2022-09-28 21:05:48 +0400

    third commit

---
+++ Day_3/Homeworks3.py
@@ -0,0 +1,7 @@
+my_name = input("enter your name: ")
+My_Surname = input("enter your Surname: ")
+My_Location = input("enter your Location: ")
+My_Birthday = input("enter your Birthday: ")
+
+if "e" in My_Surname:
+    print("Hi " + my_name)
---
+++ Day_3/lesson3.py
@@ -0,0 +1,18 @@
+# my_name = "nika"
+# my_surname = "abaiadze"
+# my_Age = 11
+# my_sentence = "my name is {}, my surname is {}, my age is {}". format(my_name, my_surname, my_Age)
+
+# print(my_sentence)
+
+# my_name = "nika"
+
+# if "e" in my_name:
+#     print("sheicavs 3-s")
+    
+# else:
+#     print("ar sheicavs e-s")
+
+# my_name = input("enter your name")
+
+# print("chemi saxelia " + my_name)
