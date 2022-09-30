c8e8b7501598ed50c57ad77713425a9eaed0027b
NufNufa <gnashgab.game007@gmail.com>
2022-09-30 22:15:47 +0400

    Third Commit

---
+++ Day_3/Homework3.py
@@ -0,0 +1,10 @@
+#input
+#output
+
+my_name = input ("Enter Your Name")
+my_surname = input ("Enter Your Surname")
+My_Password = input ("Enter Your Password")
+if ("nika123123") in My_Password:
+    print ("Welcome" " " + my_name )
+else:
+    print("Your Password Is Incorrect")
